9264ed3e590b95644ede5a64d8ac26225f852097
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-09-26 08:10:43 +0200

    login functionality completed

--- js/register.mjs
+++ js/register.mjs
@@ -5,23 +5,35 @@ import { postOptions } from "./consts/fetchOptions.mjs";
 const registerForm = document.getElementById('registerForm');
 
 registerForm.addEventListener('submit', (e) => {
-  e.preventDefault()
+  e.preventDefault();
 
-  const username = document.getElementById('register-name');
-  const password = document.getElementById('register-password');
-  const email = document.getElementById('register-email');
+  const username = document.getElementById('registerName');
+  const password = document.getElementById('registerPassword');
+  const email = document.getElementById('registerEmail');
 
   const payload = JSON.stringify({ name: username.value, email: email.value, password: password.value });
 
-  async function fetchRegister() {
-    try {
-      const user = await fetchData(registerURL, postOptions('POST', payload));
-      console.log(user)
-    }
-    catch(error) {
-      console.log(error);
-    }
-  }
+  console.log(payload);
+  // async function fetchRegister() {
+  //   try {
+  //     const user = await fetchData(registerURL, postOptions('POST', payload));
+  //     console.log(user)
+  //   }
+  //   catch(error) {
+  //     console.log(error);
+  //   }
+  // }
 
-  fetchRegister();
+  // fetchRegister();
+
+  fetch('https://nf-api.onrender.com/api/v1/social/auth/register', {
+    method: 'POST',
+    body: payload,
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  })
+  .then(response => response.json())
+  .then(json => console.log(json))
+  .catch(err => console.log(err));
 });
